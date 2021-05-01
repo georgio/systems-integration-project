930c9b614eb69c5d81eef23c707db7020c56c46a
Georgio Nicolas <me@georgio.xyz>
2021-05-01 14:56:29 +0300

    changes

--- restaurant1/Form1.cs
+++ restaurant1/Form1.cs
@@ -10,50 +10,70 @@ using System.Messaging;
 
 namespace booking
 {
-    public partial class Form1 : Form
-    {
-        MessageQueue msgQ = new MessageQueue();
-        public Form1()
-        {
-            InitializeComponent();
-            msgQ.Path = @".\private$\restaurant1";
-        }
+	public partial class Form1 : Form
+	{
+		MessageQueue msgQ = new MessageQueue();
+		public Form1()
+		{
+			InitializeComponent();
+			msgQ.Path = @".\private$\restaurant1";
+		}
 
-        private void button2_Click(object sender, System.EventArgs e)
-        {
-            System.Messaging.Message message;
-            Booking booking = new Booking();
-            StringBuilder sb;
+		private void button2_Click(object sender, System.EventArgs e)
+		{
+			System.Messaging.Message message;
 
-            try
-            {
-                ((XmlMessageFormatter)msgQ.Formatter).TargetTypes = new Type[] { typeof(Booking) };
-                message = msgQ.Receive(new TimeSpan(0, 0, 3));
-                booking = (Booking)message.Body;
+			try
+			{
+				((XmlMessageFormatter)msgQ.Formatter).TargetTypes = new Type[] { typeof(Booking) };
+				msg = msgQ.Receive(new TimeSpan(0, 0, 3));
+				Booking req = (Booking)message.Body;
+				handleRequest(req, msg.CorrelationId, msg.ResponseQueue);
+			}
+			catch (MessageQueueException)
+			{
+				MessageBox.Show("No bookings!");
+			}
 
-                sb = new StringBuilder();
-                sb.Append("Full Name: " + booking.fullName);
-                sb.Append("\n");
-                sb.Append("Persons: " + booking.persons.ToString());
-                sb.Append("\n");
-                sb.Append("Date Time: " + booking.dateTime);
-                MessageBox.Show(sb.ToString(), "Message Received!");
-            }
-            catch (MessageQueueException)
-            {
-                MessageBox.Show("No bookings!");
-            }
+		}
+		private void handleRequest(Booking req, string bookingID, MessageQueue resQ)
+		{
+			sb = new StringBuilder();
+			sb.Append("Full Name: " + booking.fullName);
+			sb.Append("\n");
+			sb.Append("Persons: " + booking.persons.ToString());
+			sb.Append("\n");
+			sb.Append("Date Time: " + booking.dateTime);
+			MessageBox.Show(sb.ToString(), "Message Received!");
+			sb.Append("\n");
+			sb.Append("\n");
+			sb.Append("Approve booking request?");
 
-        }
-        public struct Booking
-        {
-            public string fullName, dateTime;
-            public int persons;
-        }
-        public struct BookingResponse
-        {
-            public string firstName, lastName, date, time, status;
-            public int guestNumber;
-        }
-    }
+			DialogResult dr = MessageBox.Show(sb.ToString(), "Booking Response Received!", MessageBoxButtons.YesNo);
+
+			BookingResponse res;
+			res.firstName = req.firstName;
+			res.lastName = req.lastName;
+			res.date = req.date;
+			res.time = req.time;
+			res.guestNumber = req.count;
+			res.status = (dr == DialogResult.Yes) ? "Confirmed" : "Non Confirmed";
+
+			System.Messaging.Message msg = new System.Messaging.Message();
+			msg.Body = res;
+			msg.CorrelationId = bookingID;
+			resQ.Send(msg);
+			resQ.Close();
+		}
+		public struct Booking
+		{
+			public string fullName, dateTime;
+			public int persons;
+		}
+		public struct BookingResponse
+		{
+			public string firstName, lastName, date, time, status;
+			public int guestNumber;
+		}
+	}
 }
--- restaurant2/Form1.cs
+++ restaurant2/Form1.cs
@@ -10,74 +10,74 @@ using System.Messaging;
 
 namespace booking
 {
-    public partial class Form1 : Form
-    {
-        MessageQueue msgQ = new MessageQueue(@".\private$\restaurant2");
-        public Form1()
-        {
-            InitializeComponent();
-            try
-            {
+	public partial class Form1 : Form
+	{
+		MessageQueue msgQ = new MessageQueue(@".\private$\restaurant2");
+		public Form1()
+		{
+			InitializeComponent();
+			try
+			{
 
-            ((XmlMessageFormatter)msgQ.Formatter).TargetTypes = new Type[] { typeof(Booking) };
-            msgQ.ReceiveCompleted += new ReceiveCompletedEventHandler(MessageEventHandler);
-            IAsyncResult msgQResult = msgQ.BeginReceive(new TimeSpan(1, 0, 0), msgQ);
-            }
-            catch
-            {
-                
-            }
-        }
-        private void MessageEventHandler(object sender, ReceiveCompletedEventArgs e)
-        {
-            System.Messaging.Message msg = ((MessageQueue)e.AsyncResult.AsyncState).EndReceive(e.AsyncResult);
-            Booking req = (Booking)msg.Body;
-            handleRequest(req, msg.CorrelationId, msg.ResponseQueue);
-            IAsyncResult AsyncResult = ((MessageQueue)e.AsyncResult.AsyncState).BeginReceive(new TimeSpan(1, 0, 0), ((MessageQueue)e.AsyncResult.AsyncState));
-        }
-        private void handleRequest(Booking req, string bookingID, MessageQueue resQ)
-        {
-            StringBuilder sb = new StringBuilder();
+				((XmlMessageFormatter)msgQ.Formatter).TargetTypes = new Type[] { typeof(Booking) };
+				msgQ.ReceiveCompleted += new ReceiveCompletedEventHandler(MessageEventHandler);
+				IAsyncResult msgQResult = msgQ.BeginReceive(new TimeSpan(1, 0, 0), msgQ);
+			}
+			catch
+			{
 
-            sb.Append("Booked by: ");
-            sb.Append("\n\tFirst Name:" + req.firstName);
-            sb.Append("\n\tLast Name: " + req.lastName);
-            sb.Append("\n");
-            sb.Append("Count: " + req.count.ToString());
-            sb.Append("\n");
-            sb.Append("Time: " + req.time);
-            sb.Append("\n");
-            sb.Append("Date: " + req.date);
-            sb.Append("\n");
-            sb.Append("\n");
-            sb.Append("Approve booking request?");
+			}
+		}
+		private void MessageEventHandler(object sender, ReceiveCompletedEventArgs e)
+		{
+			System.Messaging.Message msg = ((MessageQueue)e.AsyncResult.AsyncState).EndReceive(e.AsyncResult);
+			Booking req = (Booking)msg.Body;
+			handleRequest(req, msg.CorrelationId, msg.ResponseQueue);
+			IAsyncResult AsyncResult = ((MessageQueue)e.AsyncResult.AsyncState).BeginReceive(new TimeSpan(1, 0, 0), ((MessageQueue)e.AsyncResult.AsyncState));
+		}
+		private void handleRequest(Booking req, string bookingID, MessageQueue resQ)
+		{
+			StringBuilder sb = new StringBuilder();
 
-            DialogResult dr = MessageBox.Show(sb.ToString(), "Booking Response Received!", MessageBoxButtons.YesNo);
+			sb.Append("Booked by: ");
+			sb.Append("\n\tFirst Name:" + req.firstName);
+			sb.Append("\n\tLast Name: " + req.lastName);
+			sb.Append("\n");
+			sb.Append("Count: " + req.count.ToString());
+			sb.Append("\n");
+			sb.Append("Time: " + req.time);
+			sb.Append("\n");
+			sb.Append("Date: " + req.date);
+			sb.Append("\n");
+			sb.Append("\n");
+			sb.Append("Approve booking request?");
 
-            BookingResponse res;
-            res.firstName = req.firstName;
-            res.lastName = req.lastName;
-            res.date = req.date;
-            res.time = req.time;
-            res.guestNumber = req.count;
-            res.status = (dr == DialogResult.Yes) ? "Confirmed" : "Non Confirmed";
+			DialogResult dr = MessageBox.Show(sb.ToString(), "Booking Response Received!", MessageBoxButtons.YesNo);
 
-            System.Messaging.Message msg = new System.Messaging.Message();
-            msg.Body = res;
-            msg.CorrelationId = bookingID;
-            resQ.Send(msg);
-            resQ.Close();
-        }
-        public struct Booking
-        {
-            public string firstName, lastName, date, time;
-            public int count;
-        }
-        public struct BookingResponse
-        {
-            public string firstName, lastName, date, time, status;
-            public int guestNumber;
-        }
+			BookingResponse res;
+			res.firstName = req.firstName;
+			res.lastName = req.lastName;
+			res.date = req.date;
+			res.time = req.time;
+			res.guestNumber = req.count;
+			res.status = (dr == DialogResult.Yes) ? "Confirmed" : "Non Confirmed";
 
-    }
+			System.Messaging.Message msg = new System.Messaging.Message();
+			msg.Body = res;
+			msg.CorrelationId = bookingID;
+			resQ.Send(msg);
+			resQ.Close();
+		}
+		public struct Booking
+		{
+			public string firstName, lastName, date, time;
+			public int count;
+		}
+		public struct BookingResponse
+		{
+			public string firstName, lastName, date, time, status;
+			public int guestNumber;
+		}
+
+	}
 }
